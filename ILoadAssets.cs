e35b588d1455dd7842d6bdd0caa4b4b9f4a6c1a8
Simon Nielsen <simonrn@gmail.com>
2025-02-24 13:17:04 +0100

    update

--- ILoadAssets.cs
+++ ILoadAssets.cs
@@ -46,20 +46,11 @@ namespace MortensWay
             sprites.Add(TileTypes.Path, content.Load<Texture2D>("Sprites\\Tiles\\dirtTile"));
             sprites.Add(TileTypes.Grass, content.Load<Texture2D>("Sprites\\Tiles\\grassTile"));
 
-            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro0"));
-            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro1"));
-            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro2"));
-            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro3"));
-            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro4"));
-            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro5"));
-            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro6"));
-            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro7"));
-
             #endregion
             #region Avatars
 
+            sprites.Add(Monstre.Goose, content.Load<Texture2D>("Sprites\\aggro0"));
             sprites.Add(MortensEnum.Bishop, content.Load<Texture2D>("Sprites\\mortenBishop0"));
-            sprites.Add(MortensEnum.Bishop, content.Load<Texture2D>("Sprites\\mortenBishop1"));
 
             #endregion
 
@@ -73,6 +64,8 @@ namespace MortensWay
         /// <param name="animations">Dictionary containing sprite-arrays with Enum as the key</param>
         private static void LoadAnimations(ContentManager content, Dictionary<Enum, Texture2D[]> animations)
         {
+
+            
 
         }
 
