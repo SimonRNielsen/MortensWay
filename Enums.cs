8ca0759698824afc61e3b2ed128b7bd8421521af
Simon Nielsen <simonrn@gmail.com>
2025-02-24 11:01:17 +0100

    removed unused usings

--- Enums.cs
+++ Enums.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MortensWay
+﻿namespace MortensWay
 {
     /// <summary>
     /// Base enum for objects like mouse or background etc.
