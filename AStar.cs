723530ac2acb947ad3a7bb5081f11df73718f041
Bjornlund <irenebjornlund@gmail.com>
2025-02-25 11:19:48 +0100

    test

--- AStar.cs
+++ AStar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpDX.Direct3D9;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace MortensWay
 {
-    internal class AStar
+    public class AStar
     {
+        Dictionary<Point, Cell> cells;
+
+        public AStar()
+        {
+        }
+
+        private HashSet<Cell> openList = new HashSet<Cell>();
     }
 }
